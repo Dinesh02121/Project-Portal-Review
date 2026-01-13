34799c3423c15ba0e9eba0187409ae2a6dd753ad
Dinesh02121 <153421362+Dinesh02121@users.noreply.github.com>
2026-01-13 23:29:32 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
-# #!/usr/bin/env bash
-# # exit on error
-# set -o errexit
+#!/usr/bin/env bash
+set -e
 
-# # Upgrade pip
-# pip install --upgrade pip
+echo "Python version:"
+python --version
 
-# # Install dependencies
-# pip install --no-cache-dir -r requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt
